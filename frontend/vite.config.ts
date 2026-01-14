dabc27a12cce25f9130eb6e8dd280116d282c1b3
thaingocphu0803 <thaingocphu0803@gmail.com>
2026-01-14 22:29:42 +0700

    add docker file production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,27 +6,29 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    host: 'reze.crm.local',
-    port: 5173,
-    open: 'login',
-    strictPort: true,
-    proxy: {
-      '/api/token': {
-        target: 'http://reze.crm.local',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/token/, '\/sanctum\/csrf-cookie'),
-      },
-      '/api': {
-        target: 'http://reze.crm.local',
-        changeOrigin: true,
-      },
-    },
-  },
+	base: '/dist/',
+	plugins: [vue(), vueDevTools()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	server: {
+		host: 'reze.crm.local',
+		port: 5173,
+		open: 'login',
+		strictPort: true,
+		proxy: {
+			'/api/token': {
+				target: 'http://reze.crm.local',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/token/, '\/sanctum\/csrf-cookie'),
+			},
+			'/api': {
+				target: 'http://reze.crm.local',
+				changeOrigin: true,
+			},
+		},
+	},
 })
+
