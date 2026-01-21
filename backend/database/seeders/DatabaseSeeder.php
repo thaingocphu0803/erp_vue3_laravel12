ba8789d94bd1af4ac0ed7b12fff9907f8eb1e5ec
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2026-01-21 10:31:05 +0700

    fix: laravel seeder error when re-build docker

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->unverified()->create();
+        User::firstOrCreate(
+        ['name' => env('ADMIN_NAME')],
+        [
+            'email' => env('ADMIN_EMAIL'),
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
+        ]);
     }
 }
