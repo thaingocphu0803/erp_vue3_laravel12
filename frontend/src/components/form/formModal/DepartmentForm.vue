f4f3e53b9446d64b69a208355cd04f191d253dd5
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2026-03-06 16:55:27 +0700

    add department index page

--- frontend/src/components/form/formModal/DepartmentForm.vue
+++ frontend/src/components/form/formModal/DepartmentForm.vue
@@ -5,7 +5,7 @@ import BaseBtn from '@/components/BaseBtn.vue'
 import AnnotationTooltip from '../AnnotationTooltip.vue'
 import ErrorAlert from '../ErrorAlert.vue'
 import departmentValidation from '@/composables/validation/useDepartmentValidation'
-import {reactive, ref } from 'vue'
+import { reactive, ref } from 'vue'
 import ListFilter from '@/components/list/ListFilter.vue'
 import Textarea from '@/components/form/Textarea.vue'
 import { useDepartmentStore } from '@/stores/department'
@@ -61,7 +61,7 @@ const getDepartmentList = async () => {
 	try {
 		loading.value = true
 		await departmentStore.departmentsFetch();
-	} catch (error:any) {
+	} catch (error: any) {
 		if (error.status === 400 || error.status === 500) {
 			getParentErrorMessage.value = error.response?.data?.messageCode
 		}
@@ -99,82 +99,54 @@ const handleCancel = () => {
 
 <template>
 
-		<Form :title @submit-form="handleCreate">
-			<error-alert :messages="errorMessage"></error-alert>
+	<Form :title @submit-form="handleCreate">
+		<error-alert :messages="errorMessage"></error-alert>
 
-			<v-row dense>
-				<v-col cols="12">
-					<Input
-						name="name"
-						:rules="departmentValidation.name"
-						v-model="departmentData.name"
-						:maxlength="defaultConfig.maxLengthName"
-						counter
-					>
+		<v-row dense>
+			<v-col cols="12">
+				<Input name="name" :rules="departmentValidation.name" v-model="departmentData.name"
+					:maxlength="defaultConfig.maxLengthName" counter>
 					<template #label>
 						{{ $t('department.input.departmentName') }}
 						<span class="text-error">*</span>
 					</template>
-					</Input>
-				</v-col>
-			</v-row>
+				</Input>
+			</v-col>
+		</v-row>
 
-			<v-row dense>
-				<v-col cols="12" md="6">
-					<Input
-						:label="$t('department.input.departmentCode')"
-						name="code"
-						v-model="departmentData.code"
-						:maxlength="defaultConfig.maxLengthCode"
-						counter
-					>
-						<template #append-inner>
-							<annotation-tooltip text="department.tooltip.codeAutoGenerate"></annotation-tooltip>
-						</template>
-					</Input>
-				</v-col>
+		<v-row dense>
+			<v-col cols="12" md="6">
+				<Input :label="$t('department.input.departmentCode')" name="code" v-model="departmentData.code"
+					:maxlength="defaultConfig.maxLengthCode" counter>
+					<template #append-inner>
+						<annotation-tooltip text="department.tooltip.codeAutoGenerate"></annotation-tooltip>
+					</template>
+				</Input>
+			</v-col>
 
-				<v-col cols="12" md="6">
-					<list-filter
-						:label="getParentErrorMessage.length ? $t(getParentErrorMessage) : $t('department.input.departmentParent')"
-						v-model="departmentData.parent_id"
-						:items="departments"
-						searchable
-						item-title="name"
-						item-value="id"
-						:loading
-						:disabled="disabledSelect"
-						@click="getDepartmentList"
-					/>
-				</v-col>
-			</v-row>
+			<v-col cols="12" md="6">
+				<list-filter
+					:label="getParentErrorMessage.length ? $t(getParentErrorMessage) : $t('department.input.departmentParent')"
+					v-model="departmentData.parent_id" :items="departments" searchable item-title="name" item-value="id"
+					:loading :disabled="disabledSelect" @click="getDepartmentList" />
+			</v-col>
+		</v-row>
 
-			<v-row dense>
-				<v-col cols="12">
-					<Textarea
-						:label="$t('department.input.departmentDesc')"
-						name="description"
-						v-model="departmentData.description"
-					></Textarea>
-				</v-col>
-			</v-row>
+		<v-row dense>
+			<v-col cols="12">
+				<Textarea :label="$t('department.input.departmentDesc')" name="description"
+					v-model="departmentData.description"></Textarea>
+			</v-col>
+		</v-row>
 
-			<!-- Actions: Cancel (red) + Create (blue) -->
-			<v-row dense justify="space-between" class="mt-2">
-				<v-col cols="auto">
-					<BaseBtn
-						title="common.btn.cancel"
-						color="red-darken-1"
-						@click.prevent="handleCancel"
-					/>
-				</v-col>
-				<v-col cols="auto">
-					<BaseBtn
-						title="common.btn.create"
-						color="primary"
-						type="submit"
-					/>
-				</v-col>
-			</v-row>
-		</Form>
+		<!-- Actions: Cancel (red) + Create (blue) -->
+		<v-row dense justify="space-between" class="mt-2">
+			<v-col cols="auto">
+				<BaseBtn title="common.btn.cancel" color="red-darken-1" @click.prevent="handleCancel" />
+			</v-col>
+			<v-col cols="auto">
+				<BaseBtn title="common.btn.create" color="primary" type="submit" />
+			</v-col>
+		</v-row>
+	</Form>
 </template>
