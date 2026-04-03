57796c23c284fae1fad43b6a091dada87112ea4a
thaingocphu0803 <167735819+thaingocphu0803@users.noreply.github.com>
2026-04-03 09:40:53 +0700

    add: vietnam province contries seeder and import data

--- backend/database/seeders/AdministrativeUnitSeeder.php
+++ backend/database/seeders/AdministrativeUnitSeeder.php
@@ -4,11 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class AdministrativeUnitSeeder extends Seeder
 {
 	/**
 	 * Run the database seeds.
 	 */
-	public function run(): void {}
+	public function run(): void {
+		$path = 'data/administrative_units.sql';
+
+		$sql = File::get(database_path($path));
+
+		DB::unprepared($sql);
+	}
 }
--- backend/database/seeders/ProvinceSeeder.php
+++ backend/database/seeders/ProvinceSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class ProvinceSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class ProvinceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $path = 'data/provinces.sql';
+
+		$sql = File::get(database_path($path));
+
+		DB::unprepared($sql);
     }
 }
